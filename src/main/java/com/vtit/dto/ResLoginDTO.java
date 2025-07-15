1653cb210a1b1886dfe0d12a77dd367f2a44bed0
HOANGVVM\Admin <hoangvu07102003@gmail.com>
2025-07-15 16:29:28 +0700

    Logout

--- src/main/java/com/vtit/dto/ResLoginDTO.java
+++ src/main/java/com/vtit/dto/ResLoginDTO.java
@@ -1,5 +1,7 @@
 package com.vtit.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ResLoginDTO {
+	@JsonProperty("access_token")
 	private String accessToken;
+	
+	@JsonProperty("user_info")
 	private UserInfo userInfo;
 	
 	@Getter
@@ -19,6 +24,14 @@ public class ResLoginDTO {
 		private long id;
 		private String username;
 		private String email;
+	}
+	
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class UserGetAccount {
+		private UserInfo userInfo;
 	}
 
 }
